81f61b1b00744935541c031e6f2ce6cf95acc654
Dvale <vale.daniel@outlook.com>
2024-11-20 11:46:48 +0100

    Ajout du design dans la page verify.php

--- verify.php
+++ verify.php
@@ -1,10 +1,44 @@
-<?php
+<!DOCTYPE html>
+<html lang="fr">
 
-require_once("./config/autoload.php");
-use ch\comem\DbManagerCRUD;
-$dbManager = new DbManagerCRUD();
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mon Site - Vérification</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+</head>
 
-if(isset($_GET['token'])){
-    $dbManager->verifyToken($_GET['token']);
-}
-?>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="page1_unprotected.php">Page 1</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="page2_protected.php">Page 2</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="container mt-5">
+        <h2 class="text-center">Vérification</h2>
+        <div class="mt-4">
+            <?php
+            require_once("./config/autoload.php");
+            use ch\comem\DbManagerCRUD;
+            $dbManager = new DbManagerCRUD();
+
+            if(isset($_GET['token'])){
+                $dbManager->verifyToken($_GET['token']);
+            }
+            ?>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
